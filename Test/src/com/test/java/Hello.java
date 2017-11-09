8a40c8f71efd1c603d2ec32d2c6961e2b0a11b30
silkplanet <silkplanet@silkplanet-it3>
2017-11-09 18:01:38 +0530

    Prakash commit

--- Test/src/com/test/java/Hello.java
+++ Test/src/com/test/java/Hello.java
@@ -4,7 +4,7 @@ public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        System.out.println("Hello This is just a test,I am Ravindra");
+        System.out.println("Hello This is just a test,I am Prakash");
 	}
 
 }
